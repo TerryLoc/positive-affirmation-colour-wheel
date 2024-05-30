5ffb98cb0fa7b7f85b6dd1de7b496a95b565ddbd
Terence Loughran <terryloughran@gmail.com>
2024-05-30 14:09:16 +0000

    Feat: add spin to wheel when mood clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,32 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   const startBtn = document.getElementById('start-btn');
+  const moodButtons = document.querySelectorAll('.mood-btn');
+  const colourWheel = document.getElementById('colourWheel1');
 
   // Event listener for the start button
-  startBtn.addEventListener('click', function () {
-    window.location.href = 'mood.html';
-  });
+  if (startBtn) {
+    startBtn.addEventListener('click', function () {
+      window.location.href = 'mood.html';
+    });
+  }
+
+  if (colourWheel && moodButtons.length > 0) {
+    moodButtons.forEach((button) => {
+      button.addEventListener('click', function () {
+        colourWheel.classList.add('spin');
+        // Remove the spin class after 8 seconds
+        setTimeout(() => {
+          colourWheel.classList.remove('spin');
+        }, 8000);
+      });
+    });
+  }
 });
 
 fetch('colours.json')
   .then((response) => response.json())
-  .then((colours) => applyColors(colours['Sad'])); // Change the desired mood
+  .then((colours) => applyColors(colours['Happy'])); // Change the desired mood
 
 /** This function creates a conic gradient with the colors for the specified mood. Each color covers a 30-degree segment of the circle, starting from 0 degrees and ending at 360 degrees.
  */
