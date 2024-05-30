5decd5830d1fa5041fea9ec7285c5df8bbdd5186
Terence Loughran <terryloughran@gmail.com>
2024-05-30 10:41:28 +0000

    Feat: the js for the wheel show mood choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,18 @@ document.addEventListener('DOMContentLoaded', function () {
     window.location.href = 'mood.html';
   });
 });
+
+// Function to apply the colours to the mood page based on the mood selected
+fetch('colours.json')
+  .then((response) => response.json())
+  .then((colours) => applyColors(colours['Angry'])); // Change 'Happy' to the desired mood
+
+/** This function creates a conic gradient with the colors for the specified mood. Each color covers a 30-degree segment of the circle, starting from 0 degrees and ending at 360 degrees.
+ */
+function applyColors(colors) {
+  const colourWheel1 = document.getElementById('colourWheel1');
+  const gradient = colors
+    .map((mood, index) => `${mood} ${index * 30}deg ${(index + 1) * 30}deg`)
+    .join(', ');
+  colourWheel1.style.background = `conic-gradient(${gradient})`;
+}
